bde8096c3a158d50a05dbcd27c0b9ed95f6bd87e
sayckl <dr.gauss196@gmail.com>
2019-07-13 12:22:58 +0300

    fixed-form-sending

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ const country = document.getElementById('country');
 const state = document.getElementById('state');
 const city = document.getElementById('city');
 const number = document.getElementById('phonenumber');
-const address = document.getElementById('adress');
+const address = document.getElementById('address');
 const about_me = document.getElementById('aboutme');
 const userslist = document.querySelector('.userlist');
 const button = document.querySelector('button');
@@ -72,50 +72,41 @@ fetch("http://localhost:3000/countries")
         })
       }
 
-function validate(event) {
+/*function validate(event) {
 
   event.preventDefault();
 
   if(!name.value.match(/^[a-zA-Z\s]*$/)) {
-    mane.classList.add("error");
+    name.classList.add("error");
+    return false;
   }
   if(!email.value.match(/^\w{1,}@\w{1,}\.\w{2,}$/)) {
     email.classList.add("error");
+    return false;
   }
   if(!number.value.match(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/)) {
     number.classList.add("error");
+    return false;
   }
-  return false;
+  if (country.value = null) {
+    country.classList.add("error");
+    return false;
+  }
+  if (state.value = null) {
+    state.classList.add("error");
+    return false;
+  }
+  if (city.value = null) {
+    city.classList.add("error");
+    return false;
+  }
   if (!address.value) {
     return address.value = 'null';
   }
   if (!about_me.value) {
-    return about_me.value = 'null';
+    return about_me.value = "null";
   }
-}
-
-user = {
-  "name": "name.value",
-  "email": "email.value",
-  "phone_number": "number.value",
-  "about_me": "about_me.value",
-  "country_id": "country.value",
-  "state_id": "state.value",
-  "city_id": "city.value"
-}
-
-button.addEventListener('click', () => {
-function sendNewUser(user) {
-return fetch('http://localhost:3000/users', {
-  method: "POST",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify('user')
-});
-}
-});
+}*/
 
 window.onload = fetch("http://localhost:3000/users")
   .then(function(response) {
@@ -150,7 +141,7 @@ function display(users){
     locationelem.innerHTML = `${elem.country_id}, ${elem.state_id}, ${elem.city_id}`;
     div.appendChild(locationelem);
     const dataelem = document.createElement('div');
-    dataelem.innerHTML = `${elem.createdAt}`;
+    dataelem.innerHTML = elem.createdAt;
     div.appendChild(dataelem);
 
     userslist.appendChild(div);
@@ -178,3 +169,73 @@ state.addEventListener('click', () => {
     return city.hidden = false
   }
 })
+
+const form = document.querySelector('.registration-form');
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const isValid = validate(event)
+  console.log(isValid)
+  if (isValid) {
+    const user = {
+      "name": name.value,
+      "email": email.value,
+      "phone_number": number.value,
+      "about_me": about_me.value,
+      "address": address.value,
+      "country_id": country.value,
+      "state_id": state.value,
+      "city_id": city.value,
+    }
+    console.log(JSON.stringify(user))
+    fetch('http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(user)
+    })
+    form.reset();
+  }
+})
+const validate = () => {
+   let isValid = true
+
+   if(!name.value.match(/^[a-zA-Z\s]*$/)) {
+     name.classList.add("error");
+     isValid = false;
+   }else{
+     name.classList.remove("error");
+   }
+   if(!email.value.match(/^\w{1,}@\w{1,}\.\w{2,}$/)) {
+     email.classList.add("error");
+     isValid = false;
+   }else{
+     email.classList.remove("error");
+   }
+   if(!number.value.match(/^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/)) {
+     number.classList.add("error");
+     isValid = false;
+   }else{
+     number.classList.remove("error");
+   }
+/*   if (country.value = "") {
+     country.classList.add("error");
+     isValid = false;
+   }
+   if (state.value = "") {
+     state.classList.add("error");
+     isValid = false;
+   }
+   if (city.value = "null") {
+     city.classList.add("error");
+     isValid = false;
+   }*/
+   if (!address.value) {
+     address.value = "null";
+   }
+   if (!about_me.value) {
+     about_me.value = "null";
+   }
+   return isValid
+ }
